5eec2be1be568495bbe52ea1e762e7cd7ac5a3ba
Dptos <dptoslaserena25@gmail.com>
2025-09-09 22:13:01 -0300

    Mapa

--- script.js
+++ script.js
@@ -41,3 +41,32 @@ sliders.forEach(slider => {
     showImage(current);
   });
 });
+// Crear mapa centrado en coordenadas iniciales
+var map = L.map('map').setView([-29.922921626875542, -71.27639819522727], 16);  
+
+// Cargar tiles de OpenStreetMap
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/">OSM</a> contributors'
+}).addTo(map);
+
+// Lista de los 4 condominios con coordenadas
+var condominios = [
+  { nombre: "Condominio Oceanic", coords: [-29.920940078467044, -71.27643362067033], idSeccion: "a1" },
+  { nombre: "Condominio Reinos de Holanda", coords: [-29.924418830510216, -71.27608490708619], idSeccion: "a2" },, 
+  { nombre: "Condominio Reinos de Italia", coords: [-29.926062795364118, -71.27649598651989], idSeccion: "a3" },
+  { nombre: "Condominio Serena Pacífico", coords: [-29.92376431818598, -71.27657730979459], idSeccion: "a4" }
+];
+
+// Agregar marcadores al mapa
+condominios.forEach(c => {
+  L.marker(c.coords)
+    .addTo(map)
+    .bindPopup(`<b>${c.nombre}</b><br>
+                 <a href=#${c.idSeccion}>
+                    Ver más detalles
+                  </a> `);
+});
+
+// Ajustar el mapa para mostrar todos los marcadores
+var grupo = L.featureGroup(condominios.map(c => L.marker(c.coords)));
+map.fitBounds(grupo.getBounds().pad(0.3));
